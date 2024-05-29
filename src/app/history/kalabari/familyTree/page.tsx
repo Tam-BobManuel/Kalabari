05ca9ab65e6487c80a5b6fd30c9c4d5c7a843ed9
Tam-BobManuel <bglorytb@gmail.com>
2024-05-29 22:13:13 +0100

    change welcome

--- src/app/history/kalabari/familyTree/page.tsx
+++ src/app/history/kalabari/familyTree/page.tsx
@@ -1,13 +1,29 @@
+"use client"
 import React from 'react';
 import data from '@/assets/data/Origin.json';
 import parse from 'html-react-parser';
 import Back from '@/components/back';
 
+import { Tree, TreeNode } from 'react-organizational-chart';
+
+
+
 function Kalabari() {
   return (
     <main className='p-2 w-[95%] mx-[auto]'>
       <Back/>
-      <h1 className='text-center item-center text-4xl mt-5 center'>Family tree coming soon...</h1>
+      <h1>Our root: A Geneological tree</h1>
+      <Tree label={<div>Mein ♂️</div>}>
+        <TreeNode label={<div>Uge ♂️</div>}>
+          <TreeNode label={<div>Perebokelekeibari</div>}>
+            <TreeNode label={<div>{"(King)"}Berembo</div>} >
+              <TreeNode label={<div>Daba</div>} >
+                <TreeNode label={<div>Daba</div>}/>
+              </TreeNode>
+            </TreeNode>
+          </TreeNode>
+        </TreeNode>
+      </Tree>
     </main>
   );
 }
