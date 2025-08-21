54efddf005c50ec90328d40a1d9cae0d46a629b4
Tam Bob-manuel <bglorytb@gmail.com>
2025-08-21 23:30:47 +0100

    fix: improve performance /photos

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState, useEffect, useRef, Suspense, CSSProperties } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -26,38 +26,36 @@ export default function Photos() {
   const [filteredImages, setFilteredImages] = useState<Imager[]>([]);
   const [offset, setOffset] = useState(0);
   const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(true);
   const bottomBoundaryRef = useRef(null);
   const fetchedImageIds = useRef(new Set<string>());
-  const [isClient, setIsClient] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [color, setColor] = useState("#ffffff");
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsClient(true);
-    }, 3000); // 3-second delay
-  }, []);
 
   const fetchImages = async () => {
-    const response = await fetch(`https://cdn.builder.io/api/v3/content/images?apiKey=${process.env.NEXT_PUBLIC_BUILDER_IO_API}&limit=8&offset=${offset}`);
-    const data = await response.json();
-    
-    if (data.results.length === 0) {
-      setHasMore(false);
-      return;
-    }
+    try {
+      const response = await fetch(`https://cdn.builder.io/api/v3/content/images?apiKey=${process.env.NEXT_PUBLIC_BUILDER_IO_API}&limit=8&offset=${offset}`);
+      const data = await response.json();
+      
+      if (data.results.length === 0) {
+        setHasMore(false);
+        return;
+      }
 
-    const newImages = data.results.filter((image: Imager) => !fetchedImageIds.current.has(image.id));
+      const newImages = data.results.filter((image: Imager) => !fetchedImageIds.current.has(image.id));
 
-    newImages.forEach((image: Imager) => {
-      fetchedImageIds.current.add(image.id);
-    });
+      newImages.forEach((image: Imager) => {
+        fetchedImageIds.current.add(image.id);
+      });
 
-    if (newImages.length > 0) {
-      setImages(prevImages => [...prevImages, ...newImages]);
-      setFilteredImages(prevImages => [...prevImages, ...newImages]);
-    } else {
-      setOffset(prevOffset => prevOffset + 8);  // Skip to next batch if all are duplicates
+      if (newImages.length > 0) {
+        setImages(prevImages => [...prevImages, ...newImages]);
+        setFilteredImages(prevImages => [...prevImages, ...newImages]);
+      } else {
+        setOffset(prevOffset => prevOffset + 8);  // Skip to next batch if all are duplicates
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -90,11 +88,11 @@ export default function Photos() {
     }
   }, [images]);
 
-  if (!isClient) {
-    return ( // Add a return statement here
+  if (loading) {
+    return (
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
         <CircleLoader
-          color={color}
+          color="#ffffff"
           loading={loading}
           cssOverride={override}
           size={150}
@@ -108,37 +106,36 @@ export default function Photos() {
   return (
     <>
       <main className={'bg-darkk p-2'} aria-label="You're at the Photos Section">
-       <div className="sticky bg-transparent top-[12%] left-0 right-0 z-10">
-        <Search images={images} setFilteredImages={setFilteredImages} />
-      </div>
-      <section className="photos-grid grid md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 gap-4 w-full">
-        <Suspense fallback={<p>Loading feed...</p>}>
-          {filteredImages?.map((photo) => (
-          <div key={photo.id} className="photo-item">
-            <Link href={photo.data.image}>
-              <div className='relative'>
-                <Image
-                  src={photo.data.image}
-                  alt={photo.data.title}
-                  className='w-full h-full'
-                  width={1665}
-                  height={100}
-                />
-                <p className='absolute bottom-0 left-0 w-full text-center bg-gray-900 text-white py-2'>{photo.data.title}</p>
+        <div className="sticky bg-transparent top-[12%] left-0 right-0 z-10">
+          <Search images={images} setFilteredImages={setFilteredImages} />
+        </div>
+        <section className="photos-grid grid md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 gap-4 w-full">
+          <Suspense fallback={<p>Loading feed...</p>}>
+            {filteredImages?.map((photo) => (
+              <div key={photo.id} className="photo-item">
+                <Link href={photo.data.image}>
+                  <div className='relative'>
+                    <Image
+                      src={photo.data.image}
+                      alt={photo.data.title}
+                      className='w-full h-full'
+                      width={1665}
+                      height={100}
+                    />
+                    <p className='absolute bottom-0 left-0 w-full text-center bg-gray-900 text-white py-2'>{photo.data.title}</p>
+                  </div>
+                </Link>
               </div>
-            </Link>
-          </div>
-        ))}
-        </Suspense>
-        
-      </section>
-      {hasMore ? (
-        <div id="page-bottom-boundary" ref={bottomBoundaryRef}></div>
-      ) : (
-        <p className="text-center">Looks like that's all we have</p>
-      )}
-    </main>
-    <Footer/>
+            ))}
+          </Suspense>
+        </section>
+        {hasMore ? (
+          <div id="page-bottom-boundary" ref={bottomBoundaryRef}></div>
+        ) : (
+          <p className="text-center">Looks like that's all we have</p>
+        )}
+      </main>
+      <Footer />
     </>
   );
 }
