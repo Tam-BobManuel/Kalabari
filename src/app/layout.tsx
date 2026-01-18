e0bf7c6a3f5155bfa0d09768bf53ae35e134b1f1
Tam Bob-manuel <bglorytb@gmail.com>
2026-01-18 12:44:30 +0100

    donations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,7 @@ export default function RootLayout({
           <Analytics />
           <SpeedInsights/>
         </LayoutProvider>
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="TamManuel" data-description="Support me on Buy me a coffee!" data-message="Thank you for your support!" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </body>
     </html>
   );
