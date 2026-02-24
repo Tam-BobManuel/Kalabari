c1e53690d27002f596e91b7dc7b3bb8a6bc919e1
Tam Bob-manuel <bglorytb@gmail.com>
2026-02-24 20:37:01 +0100

    remove coffee link on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <span className="text-white text-3xl text-shadow">Fun Facts</span>
       </Link>
     </Button>
-    <a href="https://www.buymeacoffee.com/TamManuel"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=TamManuel&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>
+    {/* <a href="https://www.buymeacoffee.com/TamManuel"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=TamManuel&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a> */}
       
   </div>
 </main>
