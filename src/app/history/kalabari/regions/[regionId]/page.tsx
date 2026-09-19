1f9657d7df68dc7d3e2464b35880d6a69f9fb211
Tam Bob-manuel <bglorytb@gmail.com>
2026-09-19 16:36:56 +0100

    switch to npm

--- src/app/history/kalabari/regions/[regionId]/page.tsx
+++ src/app/history/kalabari/regions/[regionId]/page.tsx
@@ -1,10 +1,18 @@
 import RegionDetails from "@/features/history/components/RegionDetails";
+import { notFound } from "next/navigation";
+import regions from "@/features/history/data/regions.json";
 
 export default async function RegionDetailsPage({
-  params,
+	params,
 }: {
-  params: Promise<{ regionId: string }>;
+	params: Promise<{ regionId: string }>;
 }) {
-  const { regionId } = await params;
-  return <RegionDetails regionId={regionId} />;
+	const { regionId } = await params;
+	const regionExists = regions.some((region) => region.slug === regionId);
+
+	if (!regionExists) {
+		notFound();
+	}
+
+	return <RegionDetails regionId={regionId} />;
 }
--- src/features/history/components/RegionDetails.tsx
+++ src/features/history/components/RegionDetails.tsx
@@ -1,41 +1,24 @@
-"use client";
 import Back from "@/features/shared/components/Back";
 import { notFound } from "next/navigation";
-import { useEffect, useState } from "react";
 import ReactHtmlParser from "react-html-parser";
 import regions from "../data/regions.json";
 
 export default function RegionDetails({ regionId }: { regionId: string }) {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+	const regionData = regions.find((region) => region.slug === regionId);
 
-  useEffect(() => {
-    setLoading(false);
-  }, []);
+	if (!regionData) {
+		notFound();
+	}
 
-  const regionData = regions.find((region) => region.slug === regionId);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!regionData) {
-    throw notFound();
-  }
-
-  return (
-    <main className="p-2">
-      <Back />
-      <h1 className="text-4xl text-center">
-        {regionId.charAt(0).toUpperCase() + regionId.slice(1)}
-      </h1>
-      {regionData.detail.map((item) => (
-        <p key={item.text}>{ReactHtmlParser(item.text)}</p>
-      ))}
-    </main>
-  );
+	return (
+		<main className="p-2">
+			<Back />
+			<h1 className="text-4xl text-center">
+				{regionId.charAt(0).toUpperCase() + regionId.slice(1)}
+			</h1>
+			{regionData.detail.map((item) => (
+				<p key={item.text}>{ReactHtmlParser(item.text)}</p>
+			))}
+		</main>
+	);
 }
